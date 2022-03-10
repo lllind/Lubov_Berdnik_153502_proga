62e7026c7e19cf65a8e306d270227b10a77db488
Любовь Бердник <luba@berdniik.onmicrosoft.com>
2022-03-10 12:38:31 +0300

    изменено

---
+++ lab1/_153502_Berdnik/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _153502_Berdnik
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Enter the first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a / b;
+            Console.WriteLine($"The quotient of the division of numbers : {c}");
+        }
+
+    }
+
+}
